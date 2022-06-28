f3b68a54ad79cfc20aec625a05260867ebf09786
salimza3a <salimsalimzada2002@gmail.com>
2022-06-28 18:25:59 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+dice game using React JS
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class App extends React.Component {
+
+  constructor() {
+    super()
+
+  }
+
+  render () {
+    
+    return ( <h2>Hello</h2>)
+  }
+
+}
+
+export default App
